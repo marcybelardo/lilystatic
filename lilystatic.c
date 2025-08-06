9c804414778ac30c539d97ca76a9b7c5f5bd2833
Marceline Belardo <marcelinebelardo@gmail.com>
2025-08-06 23:39:02 +0300

    restructured the directory/paths structs

--- lilystatic.c
+++ lilystatic.c
@@ -1,76 +1,33 @@
 #include <dirent.h>
-#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+static const char pkg_name[] = "lilystatic";
+static const char version[] = "0.0.1";
 
 static char *router_dir_name = "./pages";
 static char *templates_dir_name = "./templates";
 
-struct path {
+struct ls_file {
 	char *name;
 	char *contents;
-	int is_dir;
-	union {
-		// if !is_dir, pointer to the next file on the same level
-		struct path **next;
-
-		// if is_dir, pointer to a new list of files under the current one
-		struct path **subpaths;
-	};
 };
 
-struct path *init_path(char *name, int is_dir)
-{
-	struct path *new_path = malloc(sizeof(struct path));
-	new_path->name = name;
-	new_path->contents = NULL;
-	new_path->is_dir = is_dir;
+struct ls_dir {
+	struct ls_file **head;
+};
 
-	return new_path;
-}
+union ls_path {
+	struct ls_file *file;
+	struct ls_dir *dir;
+};
 
-void free_path(struct path *path)
-{
-
-}
+struct ls_list {
+	union ls_path **paths;
+};
 
 int main(int argc, char *argv[])
 {
-	struct path *router_paths = init_path("ROUTER_ROOT");
-	struct path *templates_paths = init_path("TEMPLATES_ROOT");
-
-	struct dirent *router;
-	struct dirent *templates;
-
-	DIR *d_router = opendir(router_dir_name);
-	DIR *t_router = opendir(templates_dir_name);
-
-	if (!d_router || !t_router) {
-		fprintf(stderr, "Lilystatic: folders do not exist\n");
-		exit(EXIT_FAILURE);
-	}
-
-	size_t i = 0;
-	while ((router = readdir(d_router))) {
-		printf("%s\n", router->d_name);
-		router_paths->subpaths[i] = init_path(router->d_name);
-		FILE *fp = fopen(router_paths->subpaths[i]->name, "r");
-		if (!fp)
-			continue;
-		fread(router_paths->subpaths[i]->contents, 1, 512, fp);
-		fclose(fp);
-	}
-	while ((templates = readdir(t_router))) {
-		printf("%s\n", templates->d_name);
-		templates_paths->subpaths[i] = init_path(templates->d_name);
-		FILE *fp = fopen(templates_paths->subpaths[i]->name, "r");
-		if (!fp)
-			continue;
-		fread(templates_paths->subpaths[i]->contents, 1, 512, fp);
-		fclose(fp);
-	}
-
-
+	printf("%s v%s\n", pkg_name, version);
 	
 	return 0;
 }
