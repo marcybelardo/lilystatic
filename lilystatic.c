68e78899a8593c815c594fba776d9166fffd31d2
Marceline Belardo <marcy@marcelinesystems.dev>
2025-08-08 19:32:32 +0300

    getting somewhere with the file structures i think

--- lilystatic.c
+++ lilystatic.c
@@ -10,11 +10,6 @@
 static const char pkg_name[] = "lilystatic";
 static const char version[] = "0.0.1";
 
-enum LS_FILEKIND {
-	LS_REG,
-	LS_DIR
-};
-
 // static char *router_dir_name = "./pages";
 // static char *templates_dir_name = "./templates";
 
@@ -36,77 +31,98 @@ enum LS_FILEKIND {
  *     - the router root is a directory, a directory contains files or other directories
  */
 
+enum ls_kind {
+	LS_REG,
+	LS_DIR
+};
+
 struct ls_reg {
 	char *name;
 	char *contents;
 };
 
-struct ls_file {
-	struct ls_file *next;
-	union {
-		struct ls_reg *f;
-		char *dirname;
-	} inner;
-};
-
 struct ls_dir {
+	char *name;
 	struct ls_file *head;
 };
 
-char *ls_reg_read_file(char *name)
+struct ls_file {
+	struct ls_file *next;
+	enum ls_kind kind;
+	union {
+		struct ls_reg *reg;
+		struct ls_dir *dir;
+	};
+};
+
+struct ls_dir *ls_dir_init(char *name)
 {
-	return NULL;
+	struct ls_dir *newdir = malloc(sizeof(struct ls_dir));
+	newdir->name = name;
+	newdir->head = NULL;
+
+	return newdir;
 }
 
-struct ls_dir *ls_dir_init(void)
+struct ls_file *ls_file_init_reg(char *name)
 {
-	struct ls_dir *new_dir = malloc(sizeof(struct ls_dir));
-	new_dir->head = NULL;
+	struct ls_file *newfile = malloc(sizeof(struct ls_file));
+	newfile->kind = LS_REG;
+	newfile->reg->name = name;
+	newfile->reg->contents = NULL;
+	newfile->next = NULL;
 
-	return new_dir;
+	return newfile;
 }
 
-void ls_dir_append(struct ls_dir *dir, char *name, int is_dir)
+struct ls_file *ls_file_init_dir(char *name)
 {
-	struct ls_file *cur;
-	for (cur = dir->head; cur; cur = cur->next);
-	if (is_dir == LS_DIR) {
-		cur->inner.dirname = name;
-	} else {
-		cur->inner.f->name = name;
-		cur->inner.f->contents = ls_reg_read_file(name);
-	}
+	struct ls_file *newfile = malloc(sizeof(struct ls_file));
+	newfile->kind = LS_DIR;
+	newfile->dir->name = name;
+	newfile->dir->head = NULL;
+	newfile->next = NULL;
+
+	return newfile;
 }
 
-static void walk_dir(char *name)
+void ls_dir_append(struct ls_dir *dir, struct ls_file *file)
 {
-	DIR *dir;
-	struct dirent *d_entry;
+	struct ls_file *p;
+	for (p = dir->head; p->next; p = p->next);
+	p->next = file;
+}
 
-	dir = opendir(name);
-	if (!dir) {
+static void walk_dir(struct ls_dir *dir)
+{
+	DIR *dstream;
+	struct dirent *dentry;
+
+	dstream = opendir(dir->name);
+	if (!dstream) {
 		perror("walk_dir opendir");
 		return;
 	}
 
-	while ((d_entry = readdir(dir)) != NULL) {
-		if (!strcmp(".", d_entry->d_name) || !strcmp("..", d_entry->d_name))
+	while ((dentry = readdir(dstream)) != NULL) {
+		if (!strcmp(".", dentry->d_name) || !strcmp("..", dentry->d_name))
 			continue;
 
 		char path[PATH_MAX_LEN];
-		snprintf(path, sizeof(path), "%s/%s", name, d_entry->d_name);
+		snprintf(path, sizeof(path), "%s/%s", dir->name, dentry->d_name);
 		
 		struct stat sb;
 		if (stat(path, &sb) != 0) {
 			perror("walk_dir stat");
 			continue;
 		}
-		
 
 		if (S_ISDIR(sb.st_mode)) {
-			walk_dir(path);
+			struct ls_dir *innerdir = ls_dir_init(path);
+			walk_dir(innerdir);
 		} else {
 			printf("%s\n", path);
+			struct ls_file *
 		}
 	}
 }
@@ -122,7 +138,8 @@ int main(int argc, char *argv[])
 	char *name = argv[1];
 
 	printf("root: %s\n", name);
-	walk_dir(name);
+	struct ls_dir *root = ls_dir_init(name);
+	walk_dir(root);
 
 	return 0;
 }
